cd488936da169b658b6c9795fee6a62e867a87a3
Rohan Rajput <rohanrajput32843@gmail.com>
2019-09-23 15:59:33 +0530

    deploy on heroku

---
+++ server.js
@@ -0,0 +1,38 @@
+
+// Importing Modules
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// importing files
+const routes = require('./routes');
+
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 8080; // Step 1
+
+
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/my_database', {
+    useNewUrlParser: true
+});
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
+
+app.listen(PORT, () => {
+    log(`Server is starting at PORT: ${PORT}`);
+});
